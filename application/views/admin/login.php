e53ca4197d8b4d202b3bb3174e0134b96fbc9b9a
sukhchand <sukhchand.k@echidnainc.com>
2015-04-24 16:22:59 +0530

    login page

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,1 +1,26 @@
-Login Container
+<!-- Login Container -->
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="echidna_home text-center">
+			<img src="<?php echo base_url(); ?>asserts/img/logo.png" class="img-responsive" alt="logo"/>
+			<div class="welcome">
+				<h3 class="h3">WELCOME TO THE GRID</h3>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid form-container">
+		<div class="form_div">
+			<form class="form-inline">
+				<div class="form-group">
+					<div><label class="sr-only" for="exampleInputEmail3">USERNAME</label></div>
+					<input type="email" class="form-control" id="exampleInputEmail3">
+				</div>
+				<div class="form-group password">
+					<div><label class="sr-only" for="exampleInputPassword3">PASSWORD</label></div>
+					<input type="password" class="form-control" id="exampleInputPassword3">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+		</div>
+	</div>
+</div>
