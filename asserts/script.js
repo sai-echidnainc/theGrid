33b80677d68e90b01952fc8d9c8585cadd8a4544
sukhchand <sukhchand.k@echidnainc.com>
2015-04-28 14:13:21 +0530

    Logout icon added

--- asserts/script.js
+++ asserts/script.js
@@ -4,14 +4,14 @@ $(".grid-container").mouseenter(function(){
 	$(this).find(".grid_detail a").stop().slideDown("slow",function(){
 		$(this).css("display","block");
 	});
-	$(this).find(".hover_content").fadeIn();
+	$(this).find(".hover_content").stop().fadeIn();
 	$(this).find(".overlay").css("display","block");
 })
 .mouseleave(function(){
 	/*$(this).find(".grid_detail").css("position","relative");*/
 	$(this).find(".grid_detail p").stop().slideUp("slow");
 	$(this).find(".grid_detail a").stop().slideUp("slow");
-	$(this).find(".hover_content").fadeOut();
+	$(this).find(".hover_content").stop().fadeOut();
 	$(this).find(".overlay").css("display","none");
 });
 $('#picker').colpick({
@@ -27,13 +27,13 @@ $('#picker').colpick({
 $(".crd").mouseenter(function(){
     $(this).find(".card_title").css("position","absolute");
     $(this).find(".card_title").stop().slideDown("slow");
-    $(this).find(".preview_hover").fadeIn();
+    $(this).find(".preview_hover").stop().fadeIn();
     /*$(this).find(".overlay").css("display","block");*/
 })
 .mouseleave(function(){
     /*$(this).find(".grid_detail").css("position","relative");*/
     $(this).find(".card_title").stop().slideUp("slow");   
-    $(this).find(".preview_hover").fadeOut();
+    $(this).find(".preview_hover").stop().fadeOut();
     /*$(this).find(".overlay").css("display","none");*/
 });
 
