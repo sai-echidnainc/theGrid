04b9e02a01ed840b1c02e96ca6e74c578fd67904
sukhchand <sukhchand.k@echidnainc.com>
2015-04-28 15:14:18 +0530

    drag and drop

--- application/views/admin/default/scripts.php
+++ application/views/admin/default/scripts.php
@@ -2,6 +2,7 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url(); ?>asserts/bootstrap/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>asserts/Color-Picker-master/js/colpick.js"></script>
+<script src="<?php echo base_url(); ?>asserts/Dropzone/dropzone.js"></script>
 
 <?php //Angular modules ?>
 <script src="<?php echo base_url(); ?>asserts/default/js/angular.min.js"></script>
--- application/views/admin/default/styles.php
+++ application/views/admin/default/styles.php
@@ -4,6 +4,7 @@
 <link href="<?php echo base_url(); ?>asserts/default/css/ng-css.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>asserts/font/fonts.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>asserts/Color-Picker-master/css/colpick.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>asserts/Dropzone/dropzone.css" rel="stylesheet">
 
 <link href="<?php echo base_url(); ?>asserts/style.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>asserts/responsive.css" rel="stylesheet">
