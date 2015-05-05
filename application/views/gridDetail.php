ac6db66f2fa06275407da0e2b9a35d69de71c773
sai-echidnainc <saicharan.c@echidnainc.com>
2015-05-05 11:21:21 +0530

    Changes by charan

--- application/views/gridDetail.php
+++ application/views/gridDetail.php
@@ -18,7 +18,6 @@ $imageSizeArr = array(
 	<div class="row-fluid">
 		<div class="masonry-container <?php echo (isset($cards[0]['grid_font'])) ? $cards[0]['grid_font'] : '';?>">
 			<?php
-				if(isset($cards))
 				foreach ($cards as $key => $card) {
 					//echo json_encode($card['card_type']);
 					switch ($card['card_type']) {
