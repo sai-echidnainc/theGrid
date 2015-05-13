f901ee858feec42a062a388eb7f7e9a557b7ec8c
sukhchand <sukhchand.k@echidnainc.com>
2015-05-13 02:40:14 -0700

    meta tag

--- application/views/base_templets/header.php
+++ application/views/base_templets/header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo isset($page_title) ? $page_title : "The Grid" ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php $this->load->view('base_templets/style');?>
 	</head>
 	<body class="opensans">
